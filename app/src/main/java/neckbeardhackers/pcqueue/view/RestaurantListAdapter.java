971bbb6605203925302c2c542ce277bc6b5fa566
Brandon Milton <brandonio21@linux.com>
2015-11-16 11:50:35 -0800

    Removed an unused import

--- app/src/main/java/neckbeardhackers/pcqueue/view/RestaurantListAdapter.java
+++ app/src/main/java/neckbeardhackers/pcqueue/view/RestaurantListAdapter.java
@@ -2,7 +2,6 @@ package neckbeardhackers.pcqueue.view;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
