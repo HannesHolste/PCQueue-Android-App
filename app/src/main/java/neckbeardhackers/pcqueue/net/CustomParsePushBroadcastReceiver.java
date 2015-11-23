3882ca85fa0526fd223343a545223051a4d888e7
cpacker <cpacker@ucsd.edu>
2015-11-23 11:35:05 -0800

    added functionality to receive a push of MANY restaurants #manywow

--- app/src/main/java/neckbeardhackers/pcqueue/net/CustomParsePushBroadcastReceiver.java
+++ app/src/main/java/neckbeardhackers/pcqueue/net/CustomParsePushBroadcastReceiver.java
@@ -3,10 +3,22 @@ package neckbeardhackers.pcqueue.net;
 import android.content.Context;
 import android.content.Intent;
 
+import com.parse.DeleteCallback;
+import com.parse.FindCallback;
+import com.parse.GcmBroadcastReceiver;
+import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParsePushBroadcastReceiver;
+import com.parse.ParseQuery;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import neckbeardhackers.pcqueue.model.Restaurant;
 import neckbeardhackers.pcqueue.model.RestaurantManager;
 
 /**
@@ -21,17 +33,35 @@ public class CustomParsePushBroadcastReceiver extends ParsePushBroadcastReceiver
         //super.onPushReceive(context, intent); // do not show push notification
         JSONObject pushData = getPushData(intent);
 
-        if (pushData == null || !pushData.has("restaurantObjectId")) {
+        if (pushData == null
+                || !pushData.has("restaurantObjectId") || !pushData.has("restaurantObjectIds")) {
             return;
         }
 
-        try {
-            // Extract id of changed restaurant object
-            String restaurantId = pushData.getString("restaurantObjectId");
-            RestaurantManager.getInstance().refreshIndividualRestaurantHard(restaurantId);
-        } catch (JSONException e) {
-            e.printStackTrace();
+        if (pushData.has("restaurantObjectId")) {
+            // This is for the single restaurant case
+            try {
+                // Extract id of changed restaurant object
+                String restaurantId = pushData.getString("restaurantObjectId");
+                RestaurantManager.getInstance().refreshIndividualRestaurantHard(restaurantId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else if (pushData.has("restaurantObjectIds")) {
+            // Sometimes we want to update all the restaurants at once (e.g. on a decremement)
+            // and so the server sends us all the restaurant data
+            try {
+                // Extract id of changed restaurant object
+                JSONArray jsonlist = pushData.getJSONArray("restaurantObjectIds");
+                for (int i=0; i<jsonlist.length(); i++) {
+                    String restaurantId = jsonlist.getString(i);
+                    RestaurantManager.getInstance().refreshIndividualRestaurantHard(restaurantId);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 
     private JSONObject getPushData(Intent intent) {
