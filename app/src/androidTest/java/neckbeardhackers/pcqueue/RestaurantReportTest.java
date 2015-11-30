03e87a05991034a55e180e3c693fee68e54a671b
Brianna Lam <bhlam@ucsd.edu>
2015-11-30 09:55:24 -0800

    report test so far

--- app/src/androidTest/java/neckbeardhackers/pcqueue/RestaurantReportTest.java
+++ app/src/androidTest/java/neckbeardhackers/pcqueue/RestaurantReportTest.java
@@ -7,7 +7,9 @@ import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Spinner;
 
 import org.hamcrest.Matcher;
 
@@ -15,6 +17,7 @@ import neckbeardhackers.pcqueue.model.Restaurant;
 import neckbeardhackers.pcqueue.view.ReporterActivity;
 import neckbeardhackers.pcqueue.view.RestaurantListActivity;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -28,6 +31,8 @@ public class RestaurantReportTest extends ActivityInstrumentationTestCase2<Resta
 
     private RestaurantListActivity listActivity;
 
+    private Spinner queueSelect;
+
     public RestaurantReportTest() {
         super(RestaurantListActivity.class);
     }
@@ -36,6 +41,7 @@ public class RestaurantReportTest extends ActivityInstrumentationTestCase2<Resta
         super.setUp();
         injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         listActivity = getActivity();
+        queueSelect = (Spinner) listActivity.findViewById(R.id.people_spinner);
     }
 
     public void testReportPaneOnButtonClick() {
@@ -43,8 +49,9 @@ public class RestaurantReportTest extends ActivityInstrumentationTestCase2<Resta
                 "Lemongrass", "Panda Express", "Round Table", "Rubios", "Santorini", "Shogun",
                 "Starbucks", "Subway", "Tapioca Express"};
 
+        Restaurant holder = new Restaurant();
         /* Given I am on the restaurant list, when I click on a restaurant's update button */
-        for (int i = 0; i < restaurantNames.length; i++) {
+        for (int i = 1; i < restaurantNames.length; i++) {
 
             onView(withId(R.id.RestaurantListRecycler)).perform(RecyclerViewActions.scrollToPosition(i));
             onView(withId(R.id.RestaurantListRecycler)).perform(
@@ -52,14 +59,24 @@ public class RestaurantReportTest extends ActivityInstrumentationTestCase2<Resta
                             ExtendedViewAction.clickChildViewWithId(R.id.updateButton)));
 
             /* Then the report pane should display itself with the selected restaurant's name */
-            onView(withId(R.id.reporter_restaurantName))
-                    .check(ViewAssertions.matches((withText(restaurantNames[i]))));
+            //onView(withId(R.id.reporter_restaurantName))
+             //       .check(ViewAssertions.matches((withText(restaurantNames[i]))));
 
-            onView(withId(R.id.reporter_restaurantName)).perform(
-                    RecyclerViewActions.actionOnItemAtPosition(i, ExtendedViewAction.clickChildViewWithId((R.id.reporter_updateButton))));
+            if(holder.get(restaurantNames[i]).)
+            listActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    queueSelect.requestFocus();
+                    queueSelect.setSelection(0, true);
+                }
+            });
+
+
+
+
 
             /* Given I am on the report pane, when I press the back button then I should return to the restaurant list */
-            pressBack();
+            //pressBack();
         }
     }
 }
