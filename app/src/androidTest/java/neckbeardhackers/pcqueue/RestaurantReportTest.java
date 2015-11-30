61f3254e16872c2b934dc39472496b2ab9f7ed96
Brianna Lam <bhlam@ucsd.edu>
2015-11-30 09:21:42 -0800

    report test

---
+++ app/src/androidTest/java/neckbeardhackers/pcqueue/RestaurantReportTest.java
@@ -0,0 +1,89 @@
+package neckbeardhackers.pcqueue;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+import neckbeardhackers.pcqueue.model.Restaurant;
+import neckbeardhackers.pcqueue.view.ReporterActivity;
+import neckbeardhackers.pcqueue.view.RestaurantListActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by briannalam on 11/30/15.
+ */
+public class RestaurantReportTest extends ActivityInstrumentationTestCase2<RestaurantListActivity> {
+
+    private RestaurantListActivity listActivity;
+
+    public RestaurantReportTest() {
+        super(RestaurantListActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        listActivity = getActivity();
+    }
+
+    public void testReportPaneOnButtonClick() {
+        String[] restaurantNames = {"Bombay Coast", "Burger King", "D'Lush", "Jamba Juice",
+                "Lemongrass", "Panda Express", "Round Table", "Rubios", "Santorini", "Shogun",
+                "Starbucks", "Subway", "Tapioca Express"};
+
+        /* Given I am on the restaurant list, when I click on a restaurant's update button */
+        for (int i = 0; i < restaurantNames.length; i++) {
+
+            onView(withId(R.id.RestaurantListRecycler)).perform(RecyclerViewActions.scrollToPosition(i));
+            onView(withId(R.id.RestaurantListRecycler)).perform(
+                    RecyclerViewActions.actionOnItemAtPosition(i,
+                            ExtendedViewAction.clickChildViewWithId(R.id.updateButton)));
+
+            /* Then the report pane should display itself with the selected restaurant's name */
+            onView(withId(R.id.reporter_restaurantName))
+                    .check(ViewAssertions.matches((withText(restaurantNames[i]))));
+
+            onView(withId(R.id.reporter_restaurantName)).perform(
+                    RecyclerViewActions.actionOnItemAtPosition(i, ExtendedViewAction.clickChildViewWithId((R.id.reporter_updateButton))));
+
+            /* Given I am on the report pane, when I press the back button then I should return to the restaurant list */
+            pressBack();
+        }
+    }
+}
+
+class ExtendedViewActions {
+    public static ViewAction clickChildViewWithId(final int id) {
+        return new ViewAction() {
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Click on a child view with specified ID";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View v = view.findViewById(id);
+                if (v != null)
+                    v.performClick();
+            }
+        };
+    }
+}
