466e5e0f3e042afb148133a5a9b6e2ed1c835db2
Brianna Lam <bhlam@ucsd.edu>
2015-12-03 21:49:49 -0800

    I commented it as much as I could. I don't know if they make sense, but it is my general understanding of these classes.

--- app/src/main/java/neckbeardhackers/pcqueue/model/OperatingHours.java
+++ app/src/main/java/neckbeardhackers/pcqueue/model/OperatingHours.java
@@ -8,9 +8,21 @@ import java.util.Hashtable;
 
 /**
  * Immutable class OperatingHours
+ * Description: This class will create the OperatingHours object, which will hold a table of
+ * DailyOperatingHours objects.
  */
 public final class OperatingHours {
     private final Hashtable<String, DailyOperatingHours> weeklyHours;
+
+    /**
+     * Description: The constructor to create the OperatingHours object
+     * @param weeklyHours A hashtable which will hold the DailyOperatingHours objects and the corresponding
+     *                    day of the week that the closing and opening times in the DailyOperatingHours
+     *                    object will have.
+     */
+    public OperatingHours(Hashtable<String, DailyOperatingHours> weeklyHours) {
+        this.weeklyHours = weeklyHours;
+    }
 
     /**
      * Global constant, days of the week starting at Monday = index 0.
@@ -18,19 +30,33 @@ public final class OperatingHours {
     public static final String[] DAY_NAMES = {"Sunday", "Monday", "Tuesday", "Wednesday",
             "Thursday", "Friday", "Saturday"};
 
+    /**
+     * Description: Getter function that returns a day of the week depending on the index given.
+     *
+     * @param dayNumber The appropriate index that represents a day of the week.
+     * @return The string of the day of the week.
+     */
     public static String getDayOfWeek(int dayNumber) {
         return DAY_NAMES[dayNumber];
     }
 
-    public OperatingHours(Hashtable<String, DailyOperatingHours> weeklyHours) {
-        this.weeklyHours = weeklyHours;
-    }
-
-
+    /**
+     * Description: A getter function that returns the hashtable with the opening and closes each day
+     * of the week for a restaurant.
+     *
+     * @return Hashtable of DailyOperatingHours and the day of the week string as the keys.
+     */
     public Dictionary<String, DailyOperatingHours> getOperatingHours() {
         return weeklyHours;
     }
 
+    /**
+     * Description: Getter function that searches through the hashtable and returns the DailyOperatingHours
+     * object that tells what the current day's operating hours are.
+     *
+     * @param day The day of the week that the program wants to see for DailyOperatingHours
+     * @return The DailyOperatingHours object for a particular day
+     */
     public DailyOperatingHours getOperatingHours(String day) {
         return weeklyHours.get(day);
     }
@@ -38,7 +64,11 @@ public final class OperatingHours {
     /**
      * Use Restaurant.isOpen() instead, which uses the server-side flag and therefore
      * supports constructing parse queries that sort by open time
-     * @return
+     *
+     * Description: This function will tell whether or not a restaurant is opened or closed from the
+     * current time determined.
+     * @return true The restaurant is currently open
+     * @return false The restaurant is current closed
      */
     @Deprecated
     public boolean isOpenNow(){
@@ -56,6 +86,12 @@ public final class OperatingHours {
 
     }
 
+    /**
+     * Description: Function to check if the day string given is a valid day of the week.
+     * @param day The string to check if it is valid.
+     * @return true The string is a valid day of the week.
+     * @return false The string is an invalid day of the week.
+     */
     public static boolean isValidDayName(String day) {
         for (String validDayName : OperatingHours.DAY_NAMES) {
             if (validDayName.equalsIgnoreCase(day)) {
@@ -65,6 +101,10 @@ public final class OperatingHours {
         return false;
     }
 
+    /**
+     * Description: Function to return the string of operating hours of a restaurant
+     * @return The string of operating hours for each day for a particular restaurant
+     */
     public String toString() {
         StringBuilder s = new StringBuilder();
         s.append("Hours: ");
